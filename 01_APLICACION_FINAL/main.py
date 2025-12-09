15fef5396f157a3e784643599327f86c7733ed98
Alvaro Miguel <alvaromiguelperezherrera@gmail.com>
2025-12-09 01:29:36 -0400

    add the lite version

--- 01_APLICACION_FINAL/main.py
+++ 01_APLICACION_FINAL/main.py
@@ -86,7 +86,7 @@ class OncoAIApp(ctk.CTk):
         self.create_menu_btn("Inicio", 1, True, None)
         
         # --- AQUÍ ESTÁ EL BOTÓN DE BIOSCAN EN EL SIDEBAR ---
-        self.create_menu_btn("BioScan Facial", 3, False, self.abrir_bioscan)
+        # self.create_menu_btn("BioScan Facial", 3, False, self.abrir_bioscan)
         
         self.create_menu_btn("Sobre Nosotros", 4, False, self.show_about)
 
@@ -122,7 +122,7 @@ class OncoAIApp(ctk.CTk):
 
         # Tarjetas Fila 2
         self.create_module_card(cards_grid, 1, 0, "Cáncer de Mama", "Análisis de mamografías.", "img_mama.jpg", COLOR_ACCENT_PINK, "mama", self.abrir_mama)
-        self.create_module_card(cards_grid, 1, 1, "Predicción 5 Años", "Algoritmos predictivos.", "img_futuro.jpg", COLOR_ACCENT_BLUE, "futuro", self.abrir_futuro)
+        # self.create_module_card(cards_grid, 1, 1, "Predicción 5 Años", "Algoritmos predictivos.", "img_futuro.jpg", COLOR_ACCENT_BLUE, "futuro", self.abrir_futuro)
 
     def create_module_card(self, parent, row, col, title, desc, img_name, btn_color, btn_text, command):
         card = ctk.CTkFrame(parent, fg_color=COLOR_BG_CARD, corner_radius=20)
@@ -167,21 +167,21 @@ class OncoAIApp(ctk.CTk):
         if ui_mama: ui_mama.BreastDiagnosticWindow(self, model_path, scaler_path)
         else: messagebox.showerror("Error", "Módulo Mama no cargado.")
 
-    def abrir_futuro(self):
-        model_path = os.path.join(self.models_dir, "modelo_futuro.h5")
-        scaler_path = os.path.join(self.models_dir, "scaler_futuro.pkl")
-        if not os.path.exists(model_path):
-            messagebox.showwarning("Alerta", "Modelo Futuro no encontrado.")
-            return
-        if ui_futuro: ui_futuro.FuturePredictionWindow(self, model_path, scaler_path)
-        else: messagebox.showerror("Error", "Módulo Predicción no cargado.")
+    # def abrir_futuro(self):
+    #     model_path = os.path.join(self.models_dir, "modelo_futuro.h5")
+    #     scaler_path = os.path.join(self.models_dir, "scaler_futuro.pkl")
+    #     if not os.path.exists(model_path):
+    #         messagebox.showwarning("Alerta", "Modelo Futuro no encontrado.")
+    #         return
+    #     if ui_futuro: ui_futuro.FuturePredictionWindow(self, model_path, scaler_path)
+    #     else: messagebox.showerror("Error", "Módulo Predicción no cargado.")
 
-    def abrir_bioscan(self):
-        if ui_bioscan:
-            ui_bioscan.BioScanWindow(self)
-        else:
-            # Si entras aquí es porque falló el import de cv2 o mediapipe
-            messagebox.showerror("Error Crítico", "No se pudo cargar BioScan.\n\nAsegúrate de instalar:\npip install opencv-python mediapipe")
+    # def abrir_bioscan(self):
+    #     if ui_bioscan:
+    #         ui_bioscan.BioScanWindow(self)
+    #     else:
+    #         # Si entras aquí es porque falló el import de cv2 o mediapipe
+    #         messagebox.showerror("Error Crítico", "No se pudo cargar BioScan.\n\nAsegúrate de instalar:\npip install opencv-python mediapipe")
 
     def show_about(self):
         messagebox.showinfo("Sobre OncoLabs AI",
